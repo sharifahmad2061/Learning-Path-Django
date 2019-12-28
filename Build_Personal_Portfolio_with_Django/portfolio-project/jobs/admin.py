dc9697a06531f8298d4a2d733434be7c7508153e
sharifahmad <sharifahmad2061@gmail.com>
2019-12-28 20:44:05 +0500

    model for Jobs created and migrations done.

--- Build_Personal_Portfolio_with_Django/portfolio-project/jobs/admin.py
+++ Build_Personal_Portfolio_with_Django/portfolio-project/jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Job
 # Register your models here.
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['name', 'employer', 'focal_person', 'summary']
--- Build_Personal_Portfolio_with_Django/portfolio-project/jobs/models.py
+++ Build_Personal_Portfolio_with_Django/portfolio-project/jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    name = models.CharField(max_length=150)
+    employer = models.CharField(max_length=150, null=True)
+    focal_person = models.CharField(max_length=50, null=True)
+    focal_person_contact = models.CharField(max_length=30, null=True)
+    focal_person_email = models.EmailField(null=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    summary = models.TextField()
