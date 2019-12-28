a1ce79740019456b26354c5343d248ddba62912b
sharifahmad <sharifahmad2061@gmail.com>
2019-12-28 13:08:56 +0500

    initial migration done

--- Learning_Django/pet_adoptions/models.py
+++ Learning_Django/pet_adoptions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    SEX_CHOICES = [('M','Male'),('F','Female')]
+    name = models.CharField(max_length=100)
+    admitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30,blank=True)
+    sex = models.CharField(choices=SEX_CHOICES,max_length=1,blank=True)
+    age = models.IntegerField(null=True)
+    admission_date = models.DateTimeField()
+    description = models.TextField()
+    vaccinations = models.ManyToManyField('Vaccine',blank=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
