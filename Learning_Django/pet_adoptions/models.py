320c0e3d5619f2eb15380c7a6ef7d5b7f8bec3b9
sharifahmad <sharifahmad2061@gmail.com>
2019-12-28 15:46:56 +0500

    url patterns, model and views implemented

--- Learning_Django/pet_adoptions/models.py
+++ Learning_Django/pet_adoptions/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
+
 class Pet(models.Model):
-    SEX_CHOICES = [('M','Male'),('F','Female')]
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
     name = models.CharField(max_length=100)
     admitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
-    breed = models.CharField(max_length=30,blank=True)
-    sex = models.CharField(choices=SEX_CHOICES,max_length=1,blank=True)
+    breed = models.CharField(max_length=30, blank=True)
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     age = models.IntegerField(null=True)
     admission_date = models.DateTimeField()
     description = models.TextField()
-    vaccinations = models.ManyToManyField('Vaccine',blank=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
+
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
