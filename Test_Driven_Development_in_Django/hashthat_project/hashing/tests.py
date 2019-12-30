eb9728e93e438d1a86ff9fba92b2d6f7cc0f94e5
sharifahmad <sharifahmad2061@gmail.com>
2019-12-30 11:16:29 +0500

    wrote functional tests for the application that is to be developed

--- Test_Driven_Development_in_Django/hashthat_project/hashing/tests.py
+++ Test_Driven_Development_in_Django/hashthat_project/hashing/tests.py
@@ -4,6 +4,19 @@ from selenium import webdriver
 
 class FunctionalTestCase(TestCase):
     def setUp(self):
-        browser = webdriver.Chrome()
-        browser.get('http://localhost:8000')
-        assert browser.page_source.find('install')
+        self.browser = webdriver.Chrome()
+
+    def test_homepage_exists(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('install', self.browser.page_source)
+
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161',
+                      self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
