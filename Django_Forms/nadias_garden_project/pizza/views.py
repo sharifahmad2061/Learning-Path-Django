26fe6759450677825bc1e2265a96bc99286d1ed8
sharifahmad <sharifahmad2061@gmail.com>
2019-12-30 21:49:37 +0500

    post request now provides response to user that the order has been saved.

--- Django_Forms/nadias_garden_project/pizza/views.py
+++ Django_Forms/nadias_garden_project/pizza/views.py
@@ -8,5 +8,18 @@ def index(request):
 
 
 def order(request):
-    pizza_form = PizzaForm()
-    return render(request, 'pizza/order.html', {'pizza_form': pizza_form})
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            response = 'Your {} pizza with {} & {} toppings is on its way!'.format(
+                filled_form.cleaned_data['pizza_size'],
+                filled_form.cleaned_data['topping1'],
+                filled_form.cleaned_data['topping2'])
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html', {
+                'pizza_form': new_form,
+                'order_response': response
+            })
+    else:
+        pizza_form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizza_form': pizza_form})
