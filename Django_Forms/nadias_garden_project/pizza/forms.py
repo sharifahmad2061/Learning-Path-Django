5df8ab0d5665b7a4ba1554688cb1909b8fafbb09
sharifahmad <sharifahmad2061@gmail.com>
2019-12-30 22:31:01 +0500

    converted form to model form

--- Django_Forms/nadias_garden_project/pizza/forms.py
+++ Django_Forms/nadias_garden_project/pizza/forms.py
@@ -1,10 +1,21 @@
 from django import forms
+from .models import Pizza
+
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length=30)
+#     topping2 = forms.CharField(label='Topping 2', max_length=30)
+#     pizza_size = forms.ChoiceField(label='Pizza Size',
+#                                    choices=[('small', 'Small'),
+#                                             ('medium', 'Medium'),
+#                                             ('large', 'Large')])
 
 
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='Topping 1', max_length=30)
-    topping2 = forms.CharField(label='Topping 2', max_length=30)
-    pizza_size = forms.ChoiceField(label='Pizza Size',
-                                   choices=[('small', 'Small'),
-                                            ('medium', 'Medium'),
-                                            ('large', 'Large')])
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {
+            'topping1': 'Topping 1',
+            'topping2': 'Topping 2',
+            'size': 'Size'
+        }
--- Django_Forms/nadias_garden_project/pizza/views.py
+++ Django_Forms/nadias_garden_project/pizza/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .forms import PizzaForm
+from .models import Pizza, Size
 
 
 def index(request):
@@ -11,6 +12,16 @@ def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
+            # save pizza order to db
+            pizza = Pizza()
+            size = Size()
+            pizza.topping1 = filled_form.cleaned_data['topping1']
+            pizza.topping2 = filled_form.cleaned_data['topping2']
+            size.title = filled_form.cleaned_data['pizza_size']
+            pizza.size = size
+            size.save()
+            pizza.save()
+            #send user response
             response = 'Your {} pizza with {} & {} toppings is on its way!'.format(
                 filled_form.cleaned_data['pizza_size'],
                 filled_form.cleaned_data['topping1'],
