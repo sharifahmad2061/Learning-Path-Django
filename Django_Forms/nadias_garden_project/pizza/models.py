98c860904d84bfb0531cf3318a00a8ad8a8bc2db
sharifahmad <sharifahmad2061@gmail.com>
2019-12-30 22:06:18 +0500

    views need to be connected with the model

--- Django_Forms/nadias_garden_project/pizza/models.py
+++ Django_Forms/nadias_garden_project/pizza/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class Pizza(models.Model):
+    topping1 = models.CharField(max_length=30)
+    topping2 = models.CharField(max_length=30)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
