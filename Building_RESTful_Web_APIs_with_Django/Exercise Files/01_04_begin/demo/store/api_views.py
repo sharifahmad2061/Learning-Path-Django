7e2b069537424148d508757211c768f44b8ddae6
sharifahmad <sharifahmad2061@gmail.com>
2020-01-02 21:26:13 +0500

    replaced ProductDestroy API View with ProductRetrieveUpdateDestroy API View

--- Building_RESTful_Web_APIs_with_Django/Exercise Files/01_04_begin/demo/store/api_views.py
+++ Building_RESTful_Web_APIs_with_Django/Exercise Files/01_04_begin/demo/store/api_views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
@@ -49,9 +49,10 @@ class ProductCreate(CreateAPIView):
         return super().create(request, *args, **kwargs)
 
 
-class ProductDestroy(DestroyAPIView):
+class ProductRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     lookup_field = 'id'
+    serializer_class = ProductSerializer
 
     def delete(self, request, *args, **kwargs):
         product_id = request.data.get('id')
@@ -59,4 +60,16 @@ class ProductDestroy(DestroyAPIView):
         if response.status_code == 204:
             from django.core.cache import cache
             cache.delete(f'product_data_{product_id}')
+        return response
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            from django.core.cache import cache
+            product = response.data
+            cache.set(f"product_data_{product['id']}", {
+                'name': product['name'],
+                'description': product['description'],
+                'price': product['price'],
+            })
         return response
